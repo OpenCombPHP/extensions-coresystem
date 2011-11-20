19fcb7b2b2625957c4127f7acdf104e070ed6121
alee <zq@alee2002.com>
2011-11-20 22:41:54 +0800

    + GroupsSetting

---
+++ class/user/GroupsSetting.php
@@ -0,0 +1,57 @@
+<?php
+namespace org\opencomb\coresystem\user ;
+
+use jc\bean\BeanFactory;
+
+use jc\message\Message;
+
+use org\opencomb\coresystem\mvc\controller\ControlPanel;
+
+class GroupsSetting extends ControlPanel
+{
+	public function createBeanConfig()
+	{
+		// 加载 Model group 的 Bean Config
+		$arrGroupConf = BeanFactory::singleton()->findConfig('model/group') ;
+		
+		// 在 group Config 中添加一个 hasAndBelongsToMany 关联
+		$arrGroupConf['orm']['hasAndBelongsToMany:user'] = array(
+		
+						'bridge' => 'user' ,
+		
+						'fromKeys'=>'gid' ,
+						'toBridgeKeys'=>'gid' ,
+						'fromkBridgeKeys'=>'uid' ,
+						'toKeys'=>'uid' ,
+						
+						'on' => array( 'eq', 'bridge.uid', $this->params->string('uid') ) ,
+		
+						'table' => 'user' ,
+						'colums' => 'uid' ,
+					) ;
+		
+					
+		return array(
+		
+			'model:groups' => $arrGroupConf ,
+		
+			'view:userGourps' => array(
+				'template' => 'UserGourps.html' ,
+				'class' => 'form' ,
+				
+			) 
+		) ;
+	}
+	
+	public function process()
+	{
+		// 检查参数
+		if( !$nUId = $this->params->int('uid') )
+		{
+			$this->userGourps->hideForm() ;
+			$this->userGourps->createMessage(Message::error,"缺少参数 uid") ;
+			return ;
+		}
+		
+	}
+}
