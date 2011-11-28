90f8dcd013fa7ed0dd18d0a47421dd11be9bc92d
alee <zq@alee2002.com>
2011-11-28 19:25:04 +0800

    some problems

--- class/CoreSystem.php
+++ class/CoreSystem.php
@@ -15,8 +15,6 @@ class CoreSystem extends Extension
 		$aAccessRouter->addController("org\\opencomb\\coresystem\\user\\Login",'login','') ;
 		$aAccessRouter->addController("org\\opencomb\\coresystem\\user\\Logout",'logout','') ;
 		
-		$aAccessRouter->setDefaultController('org\\opencomb\\coresystem\\user\\AvatarManager') ;
-		
 		// 权限管理器
 		PurviewManager::setSingleton( new DBPurviewManager('coresystem_purview') ) ;
 	}
