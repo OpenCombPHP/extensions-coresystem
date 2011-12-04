ec5f89af5904ab80498b9fb960c5fb508499e6f4
alee <zq@alee2002.com>
2011-12-04 23:52:13 +0800

    WelcomeControlPanel

---
+++ class/mvc/controller/WelcomeControlPanel.php
@@ -0,0 +1,16 @@
+<?php
+namespace org\opencomb\coresystem\mvc\controller ;
+
+class WelcomeControlPanel extends ControlPanel
+{
+	public function createBeanConfig()
+	{
+		return array(
+			'view:welcome' => array(
+				'template' => 'WelcomeControlPanel.html'
+			) ,
+		) ;
+	}
+}
+
+?>
