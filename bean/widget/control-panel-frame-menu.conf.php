08cc04de4258a2b9a2e2ca6d6c3d497917b4b4f1
alee <zq@alee2002.com>
2011-12-10 19:49:39 +0800

    fix some problems

--- bean/widget/control-panel-frame-menu.conf.php
+++ bean/widget/control-panel-frame-menu.conf.php
@@ -4,30 +4,30 @@ return array(
 	'id' => 'mainMenu' ,
 	'direction' => 'h' ,
 	'items' => array(
-		array(
+		'system' => array(
 			'title' => '系统' ,
 		) ,
-		array(
+		'user' => array(
 			'title' => '用户' ,
 			'link'=>'?c=org.opencomb.coresystem.user.AdminUsers' ,
 			'menu' => array(
 				'items' => array(
-					array(
+					'user-manager' => array(
 						'title'=>'用户管理' ,
 						'quote' => 'c=org.opencomb.coresystem.user.AdminUsers' ,
 						'link'=>'?c=org.opencomb.coresystem.user.AdminUsers' ,
 					) ,
 						
-					array(
+					'group' => array(
 						'title'=>'用户组' ,
 						'menu' => array(
 							'items' => array(
-								array(
+								'create-group' => array(
 										'title'=>'新建用户组'
 										, 'link'=>'?c=org.opencomb.coresystem.group.CreateGroup'
 										, 'quote'=>'c=org.opencomb.coresystem.group.CreateGroup'
 								) ,
-								array(
+								'group-manager' => array(
 										'title'=>'管理用户组'
 										,'link'=>'?c=org.opencomb.coresystem.group.AdminGroups'
 										,'quote'=>'c=org.opencomb.coresystem.group.AdminGroups'
