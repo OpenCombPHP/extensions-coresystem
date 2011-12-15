484d02cfad46c9f248b7b7a73f9585fc10e2bc22
alee <zq@alee2002.com>
2011-12-15 13:37:59 +0800

    Controller::createBeanConfig()检查是否存在和类名相同的模板文件名

--- class/mvc/controller/Controller.php
+++ class/mvc/controller/Controller.php
@@ -1,6 +1,8 @@
 <?php
 namespace org\opencomb\coresystem\mvc\controller ;
 
+use org\jecat\framework\mvc\view\UIFactory;
+
 use org\opencomb\coresystem\auth\Authorizer;
 use org\jecat\framework\mvc\view\IView;
 use org\jecat\framework\mvc\model\db\orm\Prototype;
@@ -18,18 +20,38 @@ class Controller extends JcController
 	{
 		// 用自己的类名做为模板文件名创建一个视图
 		
+		$sTemplateFilename = '' ; 
 		$sClassName = get_class($this) ;
+		$sExtensionName = ExtensionManager::singleton()->extensionNameByClass( $sClassName ) ;
+		
+		// 子目录
 		$arrSlices = explode('\\', $sClassName) ;
 		if( count($arrSlices)>3 )		// 去掉前面的3段（org/com,组织名,扩展名）
 		{
-			$arrSlices = array_slice($arrSlices,3) ; 
+			$arrSlices = array_slice($arrSlices,3) ;
+			$sFileName = implode('/',$arrSlices).'.html' ;
+			
+			// 检查模板文件是否存在
+			if( UIFactory::singleton()->sourceFileManager()->find($sFileName,$sExtensionName) )
+			{
+				$sTemplateFilename = $sFileName ;
+			}
 		}
 		
-		return array(
-			'view:view' => array(
-				'template' => implode('/',$arrSlices).'.html' ,
-			)
-		) ;
+		if(!$sTemplateFilename)
+		{
+			$sFileName = end($arrSlices).'.html' ;
+			
+			// 检查模板文件是否存在
+			if( UIFactory::singleton()->sourceFileManager()->find($sFileName,$sExtensionName) )
+			{
+				$sTemplateFilename = $sFileName ;
+			}
+		}
+		
+		return $sTemplateFilename? array(
+			'view:view' => array( 'template' => $sTemplateFilename , )
+		): array() ;
 	}
 	
     /**
