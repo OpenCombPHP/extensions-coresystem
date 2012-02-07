74ad68ece937ecbada94268fea800bc781183909
kongyuan <anubiskong@gmail.com>
2012-02-07 19:44:36 +0800

    namecard

--- class/widget/NameCard.php
+++ class/widget/NameCard.php
@@ -1,35 +1,60 @@
 <?php
 namespace org\opencomb\coresystem\widget;
 
+use org\jecat\framework\io\IOutputStream;
+use org\jecat\framework\util\IHashTable;
+use org\jecat\framework\ui\UI;
+use org\jecat\framework\mvc\model\IModel;
 use org\jecat\framework\mvc\view\widget\Widget;
 
+/**
+ * @wiki /CoreSystem/控件/名片
+ * 
+ * 名片控件,用来显示用户的信息,例如昵称,头像,积分等等.
+ * 
+ * == 使用方法 ==
+ * 需要传入一个用户的model作为信息来源,用type参数来控制显示样式,具体参数如下:
+ * {|
+ * !参数名
+ * !类型
+ * !默认值
+ * !可选
+ * !说明
+ * |-- --
+ * |model
+ * |Model
+ * |无
+ * |可选
+ * |头像控件的信息来源,必须手动显示的声明这个属性是Model类型
+ * |-- --
+ * |type
+ * |string
+ * |normal
+ * |可选
+ * |如何展示名片控件.分'simple','normal','full',3个级别,大体上决定名片控件显示信息的多少.控件内部会通过更换模板来实现这个参数的功能,如果指定了template属性,那么以template优先
+ * |-- --
+ * |template
+ * |string
+ * |'NameCard_normal.html'
+ * |可选
+ * |指定模板文件,指定这个属性后会忽略type属性
+ * |-- --
+ * |mine
+ * |bool
+ * |false
+ * |可选
+ * |是否使用正在登录的用户的信息来显示名片,该属性优先级高于model属性
+ * |}
+ * 
+ * [^]如果使用model属性来指定信息来源,并且这个属性的值是一段表达式,那么你需要另外指定属性的类型来让表达式执行,指定属性类型的代码: attr.model.type='expression'[/^]
+ * [^]如果用户使用了widget的临时模板功能,那么临时模板会比widget指定的模板优先级高[/^]
+ */
 class NameCard extends Widget {
-	public function __construct( $sId = '', $sTitle = null,  IView $aView = null) {
-		parent::__construct ( $sId, 'coresystem:NameCard.html',$sTitle, $aView );
-	}
-	
-	/**
-	 * 获取一个当前用户的信息面板控件
-	 * 
-	 * @param string $sTemplate 自定义模板
-	 * @param int $options 参数集合
-	 * @return FaceWidget
-	 */
-	static function mine($sTemplate = null)
-	{
-		$aIdModel = IdManager::fromSession()->currentId ()->userDataModel();
-		return new self($aIdModel , $sTemplate);
-	}
-	
-	/**
-	 * 根据模型参数取得一个用户信息面板 
-	 * @param IModel $aModel
-	 * @param string $sTemplate
-	 * @return FaceWidget
-	 */
-	static function instance( IModel $aModel , $sTemplate = null )
-	{
-		return new self( $aModel , $sTemplate );
+	public function __construct($aUserModel=null, $sId = '', $sTitle = null,  IView $aView = null) {
+		if($aUserModel){
+			$this->setModel($aUserModel);
+		}
+		parent::__construct ( $sId, 'coresystem:NameCard_normal.html',$sTitle, $aView );
 	}
 	
 	/**
@@ -41,7 +66,6 @@ class NameCard extends Widget {
 	}
 	
 	/**
-	 * 
 	 * @param IModel $aModel 
 	 */
 	public function setModel(IModel $aModel)
@@ -49,32 +73,63 @@ class NameCard extends Widget {
 		$this->aModel = $aModel;
 	}
 	
-	public function setUsernameVisible($bVisibel)
+	public function display(UI $aUI,IHashTable $aVariables=null,IOutputStream $aDevice=null)
 	{
-		$this->bUsernameVisible = $bVisibel;
-	}
-	public function isUsernameVisible()
-	{
-		return $this->bUsernameVisible;
-	}
-	public function username()
-	{
-		$sUsername = '';
-		if(!$this->aModel->hasData('username'))
-		{
-			$sUsername = $this->aModel->data('username');
+		if($aModel = $this->attribute('model')){
+			
+			$this->setModel($aModel);
 		}
-		return $sUsername;
+		
+		if($sDisplayType = $this->attribute('type')){
+			$this->setDisplayType($sDisplayType);
+		}
+		
+		if($sTemplateName = $this->attribute('template')){
+			$this->setTemplateName($sTemplateName);
+		}
+		parent::display($aUI, $aVariables,$aDevice);
 	}
 	
-	public function isFaceVisible()
-	{
-		return $this->bFaceVisible;
+	public function displayType(){
+		$sTemplateName = '';
+		switch ($this->templateName()){
+			case 'coresystem:NameCard_simple.html':
+				$sTemplateName = 'simple';
+				break;
+			case 'coresystem:NameCard_normal.html':
+				$sTemplateName = 'normal';
+				break;
+			case 'coresystem:NameCard_full.html':
+				$sTemplateName = 'full';
+				break;
+		}
+		return $sTemplateName;
 	}
-	public function setFaceVisible($bVisibel)
-	{
-		$this->bFaceVisible = $bVisibel;
+	public function setDisplayType($sDisplayType){
+		switch ($sDisplayType){
+			case 'simple':
+				$this->setTemplateName('coresystem:NameCard_simple.html');
+				break;
+			case 'normal':
+				$this->setTemplateName('coresystem:NameCard_normal.html');
+				break;
+			case 'full':
+				$this->setTemplateName('coresystem:NameCard_full.html');
+				break;
+			default:
+				$this->setTemplateName('coresystem:NameCard_normal.html');
+		}
 	}
+	
+	//使用正在登录中的ID来显示名片
+	public function setMine($bMine){
+		$this->bMine = (bool)$bMine;
+	}
+	
+	public function isMine(){
+		return $this->bMine;
+	}
+	
 	/**
 	 * 取得头像地址
 	 * @throws Exception 如果模型中没有需要的列
@@ -92,166 +147,12 @@ class NameCard extends Widget {
 		return $sFaceUrl;
 	}
 	
-	public function isNicknameVisible()
-	{
-		return $this->bNicknameVisible;
-	}
-	public function setNicknameVisible($bVisibel)
-	{
-		$this->bNicknameVisible = $bVisibel;
-	}
-	public function nickname()
-	{
-		$sNickname = '';
-		if(!$this->aModel->hasData('nickname'))
-		{
-			$sNickname = $this->aModel->data('nickname');
-		}
-		return $sNickname;
-	}
-	
-	public function isEmailVisible()
-	{
-		return $this->bEmailVisible;
-	}
-	public function setEmailVisible($bVisibel)
-	{
-		$this->bNicknameVisible = $bVisibel;
-	}
-	public function email()
-	{
-		$sEmail = '';
-		if(!$this->aModel->hasData('email'))
-		{
-			$sEmail = $this->aModel->data('email');
-		}
-		return $sEmail;
-	}
-	
-	public function isSexVisible()
-	{
-		return $this->bSexVisible;
-	}
-	public function setSexVisible($bVisibel)
-	{
-		$this->bSexVisible = $bVisibel;
-	}
-	public function sex()
-	{
-		$sSex = '';
-		if(!$this->aModel->hasData('sex'))
-		{
-			$sSex = $this->aModel->data('sex');
-		}
-		return $sSex;
-	}
-	
-	public function isBirthdayVisible()
-	{
-		return $this->bBirthdayVisible;
-	}
-	public function setBirthdayVisible($bVisibel)
-	{
-		$this->bBirthdayVisible = $bVisibel;
-	}
-	public function birthday()
-	{
-		$sBirthday = '';
-		if(!$this->aModel->hasData('birthday'))
-		{
-			$sBirthday = $this->aModel->data('birthday');
-		}
-		return $sBirthday;
-	}
-	
-	public function isCityVisible()
-	{
-		return $this->bCityVisible;
-	}
-	public function setCityVisible($bVisibel)
-	{
-		$this->bCityVisible = $bVisibel;
-	}
-	public function city()
-	{
-		$sCity = '';
-		if(!$this->aModel->hasData('city'))
-		{
-			$sCity = $this->aModel->data('city');
-		}
-		return $sCity;
-	}
-	
-	public function isLastLoginTimeVisible()
-	{
-		return $this->bLastLoginTimeVisible;
-	}
-	public function setLastLoginTimeVisible($bVisibel)
-	{
-		$this->bLastLoginTimeVisible = $bVisibel;
-	}
-	public function lastLoginTime()
-	{
-		$sLastLoginTime = '';
-		if(!$this->aModel->hasData('lastLoginTime'))
-		{
-			$sLastLoginTime = $this->aModel->data('lastLoginTime');
-		}
-		return $sLastLoginTime;
-	}
-	
-	public function isRegisterTimeVisible()
-	{
-		return $this->bRegisterTimeVisible;
-	}
-	public function setRegisterTimeVisible($bVisibel)
-	{
-		$this->bRegisterTimeVisible = $bVisibel;
-	}
-	public function registerTime()
-	{
-		$sRegisterTime = '';
-		if(!$this->aModel->hasData('registerTime'))
-		{
-			$sRegisterTime = $this->aModel->data('registerTime');
-		}
-		return $sRegisterTime;
-	}
-	
-	public function isActiveTimeVisible()
-	{
-		return $this->bActiveTimeVisible;
-	}
-	public function setActiveTimeVisible($bVisibel)
-	{
-		$this->bActiveTimeVisible = $bVisibel;
-	}
-	public function activeTime()
-	{
-		$sActiveTime = '';
-		if(!$this->aModel->hasData('activeTime'))
-		{
-			$sActiveTime = $this->aModel->data('activeTime');
-		}
-		return $sActiveTime;
-	}
-	
-	private $bFaceVisible = true;						//头像地址
-	private $bUsernameVisible = false;						//用户名
-	private $bNicknameVisible = false;						//昵称	
-	private $bEmailVisible = false;						//email地址
-	private $bSexVisible = false;							//性别
-	private $bBirthdayVisible = false;					//生日
-	private $bCityVisible = false;						//城市
-	private $bLastLoginTimeVisible = false;			//最后登录时间
-	private $bRegisterTimeVisible = false;				//注册时间
-	private $bActiveTimeVisible = false;				//在线时长
-	
 	/**
 	 * 数据来源
 	 * @var IModel 
 	 */
 	private $aModel;
+	private $bMine = false;
 }
 
 ?>
