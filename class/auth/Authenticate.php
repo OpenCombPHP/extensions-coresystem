18eb46d028ae98dbc610157f097705443db4267c
alee <zq@alee2002.com>
2012-04-18 10:34:27 +0800

    Authenticate::encryptPassword() 采用注册机制

--- class/auth/Authenticate.php
+++ class/auth/Authenticate.php
@@ -7,6 +7,18 @@ class Authenticate
 {
 	static public function encryptPassword(IModel $aUserModel,$sUsername,$sPlainPassword)
 	{
+		return call_user_func(self::$fnPasswordEncryptFunction,$aUserModel,$sUsername,$sPlainPassword) ;
+	}
+	
+	static public function encryptPasswordFunction(IModel $aUserModel,$sUsername,$sPlainPassword)
+	{
 		return md5( md5(md5($sUsername)) . md5($sPlainPassword) ) ;
 	}
+	
+	static public function  registerPasswordEncryptFunction($fnFunction)
+	{
+		self::$fnPasswordEncryptFunction = $fnFunction ;
+	}
+	
+	static private $fnPasswordEncryptFunction = array(__CLASS__,'encryptPasswordFunction') ;
 }
