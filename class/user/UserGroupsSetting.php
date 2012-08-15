fbf00a864ea8575aa41713e32ce5fb8ff760de78
lexdene <elephant_liu@mail.dlut.edu.cn>
2012-08-15 15:06:53 +0800

    upgrade UserGroupsSetting for rebuild of Model

--- class/user/UserGroupsSetting.php
+++ class/user/UserGroupsSetting.php
@@ -13,54 +13,12 @@ class UserGroupsSetting extends ControlPanel
 {
 	protected $arrConfig = array(
 			'title'=>'用户组设置',
-			'view' => 'UserGroupsSetting.html',
 			'perms' => array(
 				'perm.purview'=>array(
 						'name' => Id::PLATFORM_ADMIN,
 				) ,
 			) ,
 	) ;
-	
-	public function createBeanConfig()
-	{
-		// 加载 Model group 的 Bean Config
-		$arrGroupConf = BeanFactory::singleton()->findConfig('model/group','coresystem') ;
-		$arrGroupConf['list'] = true ;
-		
-		// 在 group Config 中添加一个 hasAndBelongsToMany 关联
-		$arrGroupConf['orm']['belongsTo:user'] = array(
-						'fromkeys'=>'gid' ,
-						'tokeys'=>'gid' ,
-						'keys' => array('uid','gid') ,
-						'on' => array(
-							array('eq', 'to.uid', $this->params->string('uid') )
-						) ,
-						'table' => 'group_user_link' ,
-						'colums' => 'uid' ,
-					) ;
-		
-					
-		return array(
-			'title'=>'用户组设置',
-			// models
-			'model:groups' => $arrGroupConf ,
-			'model:user' => array('config'=>'model/user') ,
-			'model:newUsrGrpLink' => array( 'orm'=>array('table'=>'group_user_link','keys'=>array('uid','gid')) ) ,
-		
-			// views
-			'view:userGroups' => array(
-				'template' => 'UserGroupsSetting.html' ,
-				'class' => 'form' ,
-				'model' => 'groups' ,
-			),
-			'perms' => array(
-					// 权限类型的许可
-					'perm.purview'=>array(
-							'name' => Id::PLATFORM_ADMIN,
-					) ,
-			) ,
-		) ;
-	}
 	
 	public function process()
 	{
@@ -79,7 +37,7 @@ class UserGroupsSetting extends ControlPanel
 			$this->createMessage(Message::error,"指定的用户无效。") ;
 			return ;
 		}
-
+		
 		$this->view()->setModel('coresystem:group')
 				->ass(array(
 					'assoc' => Prototype::belongsTo ,
@@ -94,50 +52,59 @@ class UserGroupsSetting extends ControlPanel
 		Category::buildTree($this->group) ;
 		
 		$this->doActions() ;
-		/*if( $this->userGroups->isSubmit($this->params) )
-		{
-			if(empty($this->params['groups']))
-			{
-				$this->params['groups'] = array() ;
-			}
-			
-			$sUsrGrpLinkTable = $this->newUsrGrpLink->prototype()->tableName() ;
-			
-			foreach($this->groups->childIterator() as $aGroup)
-			{
-				if( !$aGroup['user.uid'] )
-				{
-					// 新加入分组
-					if( in_array($aGroup->gid,$this->params['groups']) )
-					{
-						$this->newUsrGrpLink->uid = $this->user->uid ;
-						$this->newUsrGrpLink->gid = $aGroup->gid ;
-						if( $this->newUsrGrpLink->save() )
-						{
-							$aGroup['user.uid'] = $this->user->uid ;
-							$this->userGroups->createMessage(Message::success,"用户 %s 已经加入了分组 %s",array($this->user['username'],$aGroup['name'])) ;
-						}
-						
-					}
-				}
-				else
-				{
-					// 解除分组
-					if( !in_array($aGroup->gid,$this->params['groups']) )
-					{
-						if( DB::singleton()->execute("delete from {$sUsrGrpLinkTable} where uid='{$this->user->uid}' and gid='{$aGroup->gid}' ;") )
-						{
-							$aGroup['user.uid'] = null ;
-							$this->userGroups->createMessage(Message::success,"用户 %s 已经从分组 %s 中移除",array($this->user['username'],$aGroup['name'])) ;
-						}
-					}
-				}
-			}
-		}*/
+		
+		// reload after doActions
+		$this->group->load();
 	}
 	
 	protected function form()
 	{
 		$this->params['groups'] = $this->params['groups'] ?: array() ;
+		
+		$this->model('coresystem:group_user_link','newUsrGrpLink');
+		
+		foreach($this->group as $aGroup)
+		{
+			if( !$aGroup['user.uid'] )
+			{
+				// 新加入分组
+				if( in_array($aGroup['gid'],$this->params['groups']) )
+				{
+					$this->newUsrGrpLink->uid = $this->user->uid ;
+					$this->newUsrGrpLink->gid = $aGroup['gid'] ;
+					if( $this->newUsrGrpLink->insert() )
+					{
+						$aGroup['user.uid'] = $this->user->uid ;
+						$this->view()->createMessage(
+							Message::success,
+							"用户 %s 已经加入了分组 %s",
+							array(
+								$this->user['username'],
+								$aGroup['name']
+							)
+						) ;
+					}
+					
+				}
+			}
+			else
+			{
+				// 解除分组
+				if( !in_array($aGroup['gid'],$this->params['groups']) )
+				{
+					$this->newUsrGrpLink->delete(
+						'`gid`='.$aGroup['gid'].' and `uid`='.$this->user->uid
+					);
+					$this->view()->createMessage(
+						Message::success,
+						"用户 %s 已经从分组 %s 中移除",
+						array(
+							$this->user['username'],
+							$aGroup['name']
+						)
+					) ;
+				}
+			}
+		}
 	}
 }
